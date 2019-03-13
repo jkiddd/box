51e59925257c6a9d70b0c82685c50b061c125843
jkiddd <ivan_x_@mail.ru>
2019-03-13 21:49:45 +0600

    js слайдер

--- app-box/app/js/js/slaider.js
+++ app-box/app/js/js/slaider.js
@@ -1,22 +1,26 @@
 $(document).ready(function(){
   $('.slaider').slick(
-  {   
-      autoplay:false,
-      autoplaySpeed:2000,
-      slidesToShow: 1,
-      slidesToScroll: 1,     
+  {   slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows:false,   
       dots: false,
-      cssEase: 'linear',    
+      cssEase: 'linear',
+      asNavFor: '.slaider__mini',
+      initialSlide: 2,
+             
       }    
   );
   $('.slaider__mini').slick(
-    {   
-        autoplay:true,
-        autoplaySpeed:2000,
-        slidesToShow: 1,
-        slidesToScroll: 1,     
+    {           
+        slidesToShow: 4,
+        slidesToScroll: 1,                
         dots: false,
-        cssEase: 'linear',    
+        cssEase: 'linear',
+        asNavFor: '.slaider',
+        centerPadding:'50px',
+        focusOnSelect: true,
+        centerMode: true,
+        centerPadding:'40px',           
         }    
     );
   });
