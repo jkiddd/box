447b60a9a168293493d429d65c93705093e69f03
jkiddd <ivan_x_@mail.ru>
2019-03-14 14:58:46 +0600

    стилизация отзывы(слайдер и картинку)

---
+++ app-box/app/js/js/feedback.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.feedback__slaider').slick(
+  {   slidesToShow: 3,
+      slidesToScroll: 1,      
+      dots: false,   
+      cssEase: 'linear', 
+      centerMode: true,
+      focusOnSelect: true,            
+      }    
+  );
+
+  });
